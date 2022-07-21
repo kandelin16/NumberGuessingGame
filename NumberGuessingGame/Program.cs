43c04287102a6a25343ad015c77decf2026b4ae3
kandelin16 <78173482+kandelin16@users.noreply.github.com>
2022-07-20 22:59:47 -0600

    First commit- initialized project, created game class and rungame method.

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -4,9 +4,17 @@ namespace NumberGuessingGame
 {
     class Program
     {
+        //Main method called when project is ran
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+    class Game
+    {
+        public void RunGame()
+        {
+
         }
     }
 }
